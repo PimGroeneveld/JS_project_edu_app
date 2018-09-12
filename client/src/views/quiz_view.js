6103baff1cfc75fe2f74e11fdc982853165e665c
janeduncan <janeduncan@live.co.uk>
2018-09-12 10:54:28 +0100

    buttons

--- client/src/views/quiz_view.js
+++ client/src/views/quiz_view.js
@@ -2,79 +2,56 @@ const PubSub = require("../helpers/pub_sub.js");
 const Quiz = require("../models/quiz.js")
 
 const QuizView = function(container, question){
-  this.questionsContainer = container;
-  this.question = question;
+ this.questionsContainer = container;
+ this.question = question;
 }
 
 QuizView.prototype.render = function (flashcard) {
-  const questionContainer = document.createElement('div');
-  questionContainer.classList.add('questionContainer');
+ const questionContainer = document.createElement('div');
+ questionContainer.classList.add('questionContainer');
 
-  const foreign = this.createForeignPhrase();
-  questionContainer.appendChild(foreign);
+ const foreign = this.createForeignPhrase();
+ questionContainer.appendChild(foreign);
 
-  const textBox = document.createElement('input');
-  questionContainer.appendChild(textBox);
+ const textBox = document.createElement('input');
+ questionContainer.appendChild(textBox);
 
-  const checkAnswerButton = document.createElement('button');
-  checkAnswerButton.classList.add('checkButton');
-  buttonTitle = document.createTextNode("Check answer");
-  checkAnswerButton.appendChild(buttonTitle);
-  questionContainer.appendChild(checkAnswerButton);
-  checkAnswerButton.addEventListener('click', (event) => {
-    const quiz = new Quiz();
-    quiz.checkAnswer(textBox.value, this.question[0]);
-  });
+ const checkAnswerButton = document.createElement('button');
+ checkAnswerButton.classList.add('checkButton');
+ buttonTitle = document.createTextNode("Check answer");
+ checkAnswerButton.appendChild(buttonTitle);
+ questionContainer.appendChild(checkAnswerButton);
+ checkAnswerButton.addEventListener('click', (event) => {
+   const quiz = new Quiz();
+   quiz.checkAnswer(textBox.value.toLowerCase(), this.question[0].toLowerCase());
+ });
 
-
-  this.questionsContainer.appendChild(questionContainer);
+ this.questionsContainer.appendChild(questionContainer);
 };
 
 QuizView.prototype.createForeignPhrase = function() {
-  const foreign = document.createElement('p');
-  foreign.textContent = this.question[1];
-  return foreign;
+ const foreign = document.createElement('p');
+ foreign.textContent = this.question[1];
+ return foreign;
 };
 
 QuizView.prototype.renderCorrect = function () {
-
-  const questionContainer = document.createElement('div');
-  const text = document.createElement('h2');
-  // text.textContent = "Correct!";
-  text.innerHTML = '<font color="green">Correct!</font>';
-  questionContainer.appendChild(text);
-  this.questionsContainer.appendChild(questionContainer);
+ const questionContainer = document.createElement('div');
+ questionContainer.classList.add('questionContainer');
+ const correct = document.createElement('p');
+ correct.innerHTML = '<font color="green">Correct!</font><br><img src="images/smiley.png" width="50px" height="50px"/>';
+ questionContainer.appendChild(correct);
+ this.questionsContainer.appendChild(questionContainer);
 };
 
 QuizView.prototype.renderFalse = function () {
 
-  const questionContainer = document.createElement('div');
-  const text = document.createElement('h2');
-  // text.textContent = "Try again";
-  text.innerHTML = '<font color="red">Try again</font>';
-  questionContainer.appendChild(text);
-  this.questionsContainer.appendChild(questionContainer);
-};
-
-module.exports = QuizView;
-
-
-QuizView.prototype.renderCorrect = function () {
-
-  const questionContainer = document.createElement('div');
-  const foreign = document.createElement('h2');
-  foreign.textContent = "Hello, World"
-  questionContainer.appendChild(foreign);
-  this.questionsContainer.appendChild(questionContainer);
-};
-
-QuizView.prototype.renderFalse = function () {
-
-  const questionContainer = document.createElement('div');
-  const foreign = document.createElement('h2');
-  foreign.textContent = "Goodbye, World"
-  questionContainer.appendChild(foreign);
-  this.questionsContainer.appendChild(questionContainer);
+ const questionContainer = document.createElement('div');
+ questionContainer.classList.add('questionContainer');
+ const wrong = document.createElement('p');
+ wrong.innerHTML = '<font color="red">Try again</font><br><img src="images/frownie.png" width="50px" height="50px"/>';
+ questionContainer.appendChild(wrong);
+ this.questionsContainer.appendChild(questionContainer);
 };
 
 module.exports = QuizView;
